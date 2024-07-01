5824ead7961cf12428c0db478d61273e4aa5d7c5
JamesLee <jameslee82027@gmail.com>
2024-07-02 00:39:14 +0900

    react removed from toastr provider

--- src/providers/toastrProvider.tsx
+++ src/providers/toastrProvider.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react'
 import { Toaster } from "react-hot-toast";
 
 export default function ToastrProvider({ children, position }: { children: ReactNode, position: 'top-center' | 'bottom-center' }) {
