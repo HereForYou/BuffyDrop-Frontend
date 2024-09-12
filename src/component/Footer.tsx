55ae257cba28b8d6df90d65ec8bc9232185e709a
DavidLamos <davidlamos4453@gmail.com>
2024-09-12 20:43:50 +0900

    start project

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,61 +1,60 @@
 interface IFooterProps {
-  tab: string;
-  setTab: (status: string) => void;
+  tab: string
+  setTab: (status: string) => void
 }
 const tabs = [
   {
-    id: "Leaderboard",
-    name: "Leaderboard",
-    img: "rank.svg"
+    id: 'Exchange',
+    name: 'Home',
+    img: 'home.svg'
   },
   {
-    id: "Friends",
-    name: "Referrals",
-    img: "referral.svg"
-  },
-  {
-    id: "Exchange",
-    name: "Home",
-    img: "home.svg"
-  },
-  {
-    id: "Task",
-    name: "Daily Tasks",
-    img: "task.svg"
-  },
-  {
-    id: "Mine",
-    name: "Miner",
-    img: "badge.svg"
+    id: 'Leaderboard',
+    name: 'Leaderboard',
+    img: 'rank.svg'
   },
 
+  {
+    id: 'Friends',
+    name: 'Referrals',
+    img: 'referral.svg'
+  }
+  // {
+  //   id: "Task",
+  //   name: "Daily Tasks",
+  //   img: "task.svg"
+  // },
+  // {
+  //   id: "Mine",
+  //   name: "Miner",
+  //   img: "badge.svg"
+  // },
 ]
 const Footer: React.FC<IFooterProps> = ({ tab, setTab }) => {
   const handleClick = (tab: string) => {
-    setTab(tab);
+    setTab(tab)
   }
   return (
     // <div className="footer grid grid-cols-5 justify-between absolute z-10 h-[60px] w-full md:w-[400px] md:mx-auto bottom-0 items-center px-4 rounded-2xl">
-    <div className="footer grid grid-cols-5 justify-between absolute z-10 h-[60px] w-full bottom-0 items-center px-4 rounded-2xl">
-      {
-        tabs.map((item, index) => (
-          <div key={index} onClick={() => handleClick(item.id)} className={`flex flex-col items-center justify-center cursor-pointer transform origin-bottom transition 
-            ${item.id === tab
-              ? "scale-[110%]"
-              : "opacity-60"
-            }`}>
-            <div className={`flex flex-col items-center justify-center font-extrabold ${item.id === tab && 'border-b-2 border-[#fff]'} gap-1`}>
-              <img
-                src={item.img}
-                alt="play"
-                className="w-5 h-5"
-              />
-              <h3 className="text-[10px]">{item.name}</h3>
-            </div>
+    <div className='footer grid grid-cols-3 justify-between absolute z-10 h-[60px] w-full bottom-0 items-center px-4 rounded-2xl'>
+      {tabs.map((item, index) => (
+        <div
+          key={index}
+          onClick={() => handleClick(item.id)}
+          className={`flex flex-col items-center justify-center cursor-pointer transform origin-bottom transition 
+            ${item.id === tab ? 'scale-[110%]' : 'opacity-60'}`}
+        >
+          <div
+            className={`flex flex-col items-center justify-center font-extrabold ${
+              item.id === tab && 'border-b-2 border-[#fff]'
+            } gap-1`}
+          >
+            <img src={item.img} alt='play' className='w-5 h-5' />
+            <h3 className='text-[10px]'>{item.name}</h3>
           </div>
-        ))
-      }
-    </div >
-  );
+        </div>
+      ))}
+    </div>
+  )
 }
-export default Footer;
+export default Footer
