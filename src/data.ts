7210889669cc9f60ec28cc63a8471347c0a34009
DavidLamos <davidlamos4453@gmail.com>
2024-09-12 11:21:21 +0900

    token

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
-// const ENDPOINT = 'http://localhost:5000';
-const ENDPOINT = 'https://app.bleggs.com';
+const ENDPOINT = 'http://localhost:5000';
+// const ENDPOINT = 'https://app.bleggs.com';
 const BOT_TOKEN = '7233907096:AAFXzUYCRmFFN9jJmnNE-gL56awzOh9heCs';
 
 export { ENDPOINT, BOT_TOKEN };
