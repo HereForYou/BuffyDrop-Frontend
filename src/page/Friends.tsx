b4a1be3a2d6f0ff7642f152230c07dedbd1f2874
JamesLee <jameslee82027@gmail.com>
2024-09-14 06:12:54 +0900

    fix:leaderboard

--- src/page/Friends.tsx
+++ src/page/Friends.tsx
@@ -1,44 +1,53 @@
-import { useState, useEffect, useRef } from "react";
-import axios from "axios";
-import Loader from "../component/Loader";
-import { toast } from 'react-hot-toast';
-import { faClone, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import InviteCard from "../component/InviteCard";
-import FriendCard from "../component/FriendCard";
+import { useState, useEffect, useRef } from 'react'
+import axios from 'axios'
+import Loader from '../component/Loader'
+import { toast } from 'react-hot-toast'
+import { faClone, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import InviteCard from '../component/InviteCard'
+import FriendCard from '../component/FriendCard'
 
-import { ENDPOINT } from "../data";
+import { ENDPOINT } from '../data'
 
-const desText = `\nJoin me on the exciting journey with BLEGGS! 🚀 Click the LAUNCH button to start mining tokens and turn your efforts into real money. 💰 Let's mine and earn together!`;
+const desText = `\nJoin me on the exciting journey with BLEGGS! 🚀 Click the LAUNCH button to start mining tokens and turn your efforts into real money. 💰 Let's mine and earn together!`
 
-const Friends = ({ user, inviteRevenue }: { user: any, inviteRevenue: number }) => {
-  const [showModal, setShowModal] = useState<boolean>(false);
-  const [inviteLink, setInviteLink] = useState<string>("");
-  const [friends, setFriends] = useState<object[]>([]);
+const Friends = ({
+  user,
+  inviteRevenue
+}: {
+  user: any
+  inviteRevenue: number
+}) => {
+  const [showModal, setShowModal] = useState<boolean>(false)
+  const [inviteLink, setInviteLink] = useState<string>('')
+  const [friends, setFriends] = useState<object[]>([])
   // const [totalFriendPoints, setTotalFriendPoints] = useState<number>(0.0);
-  const hasShownWarningRef = useRef(false);
+  const hasShownWarningRef = useRef(false)
 
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(true)
   useEffect(() => {
     if (!hasShownWarningRef.current && user) {
       //https:reques
-      setLoading(true);
-      axios.get(`${ENDPOINT}/api/user/friend/${user?.id}`)
-        .then((res) => {
-          let userInfo = res.data;
-          setInviteLink(userInfo.inviteLink);
-          setFriends(userInfo.friendsInfo);
-          setLoading(false);
+      setLoading(true)
+      axios
+        .get(`${ENDPOINT}/api/user/friend/${user?.id}`)
+        .then(res => {
+          let userInfo = res.data
+          console.log('userInfo', userInfo)
+
+          setInviteLink(userInfo.inviteLink)
+          setFriends(userInfo.friendsInfo)
+          setLoading(false)
         })
-        .catch((err) => {
-          console.error(err);
+        .catch(err => {
+          console.error(err)
           // toast.error("Something Went Wrong!");
         })
-      hasShownWarningRef.current = true;
+      hasShownWarningRef.current = true
     }
   }, [])
   //clipBoard
-  function legacyCopy(value: string) {
+  function legacyCopy (value: string) {
     const ta = document.createElement('textarea')
     ta.value = value ?? ''
     ta.style.position = 'absolute'
@@ -51,85 +60,99 @@ const Friends = ({ user, inviteRevenue }: { user: any, inviteRevenue: number }) 
   const handleClipBoardCopy = async () => {
     legacyCopy(`
 https://t.me/Bleggesminer_bot/Bleggs?startapp=${inviteLink}
-${desText}`);
-    toast.success("Successfully Copied!");
+${desText}`)
+    toast.success('Successfully Copied!')
   }
   return (
-    <div className="pb-[40px]">
-      <h1 className="text-xl font-bold text-[#acacac] pt-10">Invite friends</h1>
-      <h1 className="text-xl font-bold text-[#acacac] pb-2">and get more BUFFYS</h1>
-      <div className="w-full flex justify-center">
-        <img src="/friends_bg.png" alt="friends_bg" className="h-full px-10 py-2 "/>
+    <div className='pb-[40px]'>
+      <h1 className='text-xl font-bold text-[#acacac] pt-10'>Invite friends</h1>
+      <h1 className='text-xl font-bold text-[#acacac] pb-2'>
+        and get more BUFFYS
+      </h1>
+      <div className='w-full flex justify-center'>
+        <img
+          src='/friends_bg.png'
+          alt='friends_bg'
+          className='h-full px-10 py-2 '
+        />
       </div>
-      <div className="flex flex-col justify-between items-start px-6 text-[#acacac]">
-        <h3 className="text-left text-xl font-bold">
-        <span>{friends.length+2}</span> friends
+      <div className='flex flex-col justify-between items-start px-6 text-[#acacac]'>
+        <h3 className='text-left text-xl font-bold'>
+          <span>{friends.length + 2}</span> friends
         </h3>
       </div>
-      <div className="mt-3 space-y-2 overflow-auto h-[40vh]">
-        {
-          loading ? (
-            <div className="flex items-center justify-center w-full">
-              <Loader width="30" />
-            </div>
-          ) : (
-            friends.length > 0 ? (
-              friends.map((friend: any) => {
-                return (                  
-                  <FriendCard
-                    key={friend.userName}
-                    name={friend.userName}
-                    value={friend.totalPoints}
-                  />
-                )
-              })
-            ) : (
-              <div>
-                <h4 className="pt-1 text-[#acacac]">
-                  {/* No Friends yet */}
-                  <FriendCard
-                    key={"super123"}
-                    name={"super123"}
-                    value={312542}
-                  />
-                  <FriendCard
-                    key={"submit333"}
-                    name={"submit333"}
-                    value={222542}
-                  />
-                </h4>
-              </div>
+      <div className='mt-3 space-y-2 overflow-auto h-[40vh]'>
+        {loading ? (
+          <div className='flex items-center justify-center w-full'>
+            <Loader width='30' />
+          </div>
+        ) : friends.length > 0 ? (
+          friends.map((friend: any) => {
+            return (
+              <FriendCard
+                key={friend.userName}
+                name={friend.userName}
+                value={friend.totalPoints}
+              />
             )
-          )
-        }
-
+          })
+        ) : (
+          <div>
+            <h4 className='pt-1 text-[#acacac]'>
+              {/* No Friends yet */}
+              <FriendCard key={'super123'} name={'super123'} value={312542} />
+              <FriendCard key={'submit333'} name={'submit333'} value={222542} />
+            </h4>
+          </div>
+        )}
       </div>
-      <div className="absolute bottom-[10vh] w-full px-8">
-        <InviteCard title="Invite Friends" profit={inviteRevenue} setShowModal={setShowModal} />
+      <div className='absolute bottom-[10vh] w-full px-8'>
+        <InviteCard
+          title='Invite Friends'
+          profit={inviteRevenue}
+          setShowModal={setShowModal}
+        />
       </div>
-      <div className={`absolute bg-[#4b37dd] rounded-3xl flex flex-col w-full right-0 text-[#acacac] px-4 py-2 pb-8 gap-4 transition-all duration-500 ease-out transform ${showModal ? 'bottom-[40px]' : 'bottom-[-400px]'}`}>
-        <div className="h-[5px] rounded-full w-[80px] bg-black opacity-80 self-center"></div>
-        <h2 className="text-[32px]">Invite Friends</h2>
-        <h4 className="text-[16px] text-gray opacity-70">You have <span className="text-yellow-400 font-bold">Unlimited</span> invitations available</h4>
-        <a href={`https://t.me/share/url?url=https://t.me/Bleggesminer_bot/Bleggs?startapp=${inviteLink}&text=${desText}`} target="blank" className="bg-[#110d33] p-[4px] rounded-full text-[#acacac]  transition relative duration-200 hover:translate-y-[2px]">
-          <div className=" cursor-pointer py-2 hover:bg-indigo-[#110d33] rounded-full">
-            <FontAwesomeIcon icon={faPaperPlane} className="mr-5" />
+      <div
+        className={`absolute bg-[#4b37dd] rounded-3xl flex flex-col w-full right-0 text-[#acacac] px-4 py-2 pb-8 gap-4 transition-all duration-500 ease-out transform ${
+          showModal ? 'bottom-[40px]' : 'bottom-[-400px]'
+        }`}
+      >
+        <div className='h-[5px] rounded-full w-[80px] bg-black opacity-80 self-center'></div>
+        <h2 className='text-[32px]'>Invite Friends</h2>
+        <h4 className='text-[16px] text-gray opacity-70'>
+          You have <span className='text-yellow-400 font-bold'>Unlimited</span>{' '}
+          invitations available
+        </h4>
+        <a
+          href={`https://t.me/share/url?url=https://t.me/Bleggesminer_bot/Bleggs?startapp=${inviteLink}&text=${desText}`}
+          target='blank'
+          className='bg-[#110d33] p-[4px] rounded-full text-[#acacac]  transition relative duration-200 hover:translate-y-[2px]'
+        >
+          <div className=' cursor-pointer py-2 hover:bg-indigo-[#110d33] rounded-full'>
+            <FontAwesomeIcon icon={faPaperPlane} className='mr-5' />
             Send
           </div>
         </a>
-        <div className="bg-[#110d33] p-[4px] rounded-full transition relative duration-200 hover:translate-y-[2px]">
-          <div onClick={handleClipBoardCopy} className=" cursor-pointer hover:bg-indigo-[#110d33] py-2 rounded-full ">
-            <FontAwesomeIcon icon={faClone} className="mr-5" />
+        <div className='bg-[#110d33] p-[4px] rounded-full transition relative duration-200 hover:translate-y-[2px]'>
+          <div
+            onClick={handleClipBoardCopy}
+            className=' cursor-pointer hover:bg-indigo-[#110d33] py-2 rounded-full '
+          >
+            <FontAwesomeIcon icon={faClone} className='mr-5' />
             Copy Link
           </div>
         </div>
-        <div className="bg-[#110d33] p-[4px] rounded-full transition relative duration-200 hover:translate-y-[2px]">
-          <div onClick={() => setShowModal(false)} className=" cursor-pointer hover:bg-indigo-[#110d33] py-2 rounded-full ">
+        <div className='bg-[#110d33] p-[4px] rounded-full transition relative duration-200 hover:translate-y-[2px]'>
+          <div
+            onClick={() => setShowModal(false)}
+            className=' cursor-pointer hover:bg-indigo-[#110d33] py-2 rounded-full '
+          >
             Cancel
           </div>
         </div>
       </div>
     </div>
-  );
-};
-export default Friends;
+  )
+}
+export default Friends
--- src/page/Leaderboard.tsx
+++ src/page/Leaderboard.tsx
@@ -1,256 +1,133 @@
-import axios from "axios";
-import React, { useEffect, useState, useRef } from "react";
-import { toast } from "react-hot-toast";
-import Loader from "../component/Loader";
-import { ENDPOINT } from "../data";
-import { formatNumberWithCommas } from "../utils/functions";
+import axios from 'axios'
+import React, { useEffect, useState, useRef } from 'react'
+import { toast } from 'react-hot-toast'
+import Loader from '../component/Loader'
+import { ENDPOINT } from '../data'
+import { formatNumberWithCommas } from '../utils/functions'
 
 interface ILeaderboardProps {
-  user: any;
+  user: any
 }
 
 const Leaderboard: React.FC<ILeaderboardProps> = ({ user }) => {
-  const [users, setUsers] = useState<object[]>([]);
-  const [curUser, setCurUser] = useState<any>({});
-  const [ranking, setRaking] = useState<number>(0);
-  const hasShownWarningRef = useRef(false);
-  
-  const [loading, setLoading] = useState<boolean>(true);
-  
+  const [users, setUsers] = useState<object[]>([])
+  const [curUser, setCurUser] = useState<any>({})
+  const [ranking, setRaking] = useState<number>(0)
+  const hasShownWarningRef = useRef(false)
+
+  const [loading, setLoading] = useState<boolean>(true)
+
   useEffect(() => {
     if (!hasShownWarningRef.current && user) {
-      setLoading(true);
+      setLoading(true)
       // axios.get(`${ENDPOINT}/api/user/all/${user.id}`)
-      axios.get(`${ENDPOINT}/api/user/top/${user.id}?num=100`)
-      .then((res) => {
-          let userInfo = res.data;
-          setUsers(userInfo.topUsers);
-          setCurUser(userInfo.curUser);
-          setRaking(userInfo.ranking);
-          setLoading(false);
+      axios
+        .get(`${ENDPOINT}/api/user/top/${user.id}?num=100`)
+        .then(res => {
+          let userInfo = res.data
+
+          setUsers(userInfo.topUsers)
+          setCurUser(userInfo.curUser)
+          setRaking(userInfo.ranking)
+          setLoading(false)
         })
-        .catch((err) => {
-          console.error(err);
-          toast.error("Something Went Wrong!");
+        .catch(err => {
+          console.error(err)
+          toast.error('Something Went Wrong!')
         })
-      hasShownWarningRef.current = true;
+      hasShownWarningRef.current = true
     }
   }, [])
 
   return (
-    <div className="h-full flex flex-col text-center items-center justify-between py-2 px-6">
-      <div className="w-full">
+    <div className='h-full flex flex-col text-center items-center justify-between py-2 px-6'>
+      <div className='w-full'>
         {/* <div className="customCard-container w-full"> */}
-          <div className="group pt-6 transition relative duration-300 cursor-default hover:shadow-[0 -8px 0px 0px #2196f3]">
-            {/* <h2 className="text-[24px] font-extrabold">Top {users.length} $BLEGGS Miners</h2> */}
-            <h2 className="text-2xl text-[#acacac] font-extrabold">Telegram Wall of Fame</h2>
-          </div >
+        <div className='group pt-6 transition relative duration-300 cursor-default hover:shadow-[0 -8px 0px 0px #2196f3]'>
+          {/* <h2 className="text-[24px] font-extrabold">Top {users.length} $BLEGGS Miners</h2> */}
+          <h2 className='text-2xl text-[#acacac] font-extrabold'>
+            Telegram Wall of Fame
+          </h2>
+        </div>
         {/* </div > */}
         {loading ? (
-          <div className="w-full flex items-center justify-center">
-            <Loader width="30" />
+          <div className='w-full flex items-center justify-center'>
+            <Loader width='30' />
           </div>
         ) : (
-          <div className={`flex my-3 px-3 py-1 items-center text-[#acacac] bg-[#110d33] rounded-lg w-full`}>
-            <div className="relative h-10 w-10 overflow-hidden flex items-center">
-              <img src="/logo.png" alt="avatar" className="w-7 h-7 rounded-full bg-white" />
-            </div>
-            <div className="w-full">
-              <p className="text-xs text-start pl-2">{curUser?.userName}</p>
-              <p className="text-[8px] text-start pl-2">
-                {formatNumberWithCommas(curUser?.totalPoints.toFixed(2))+" BUFFYS"}
+          <div
+            className={`flex my-3 px-3 py-2 items-center text-[#acacac] bg-[#110d33] rounded-lg w-full`}
+          >
+            <div className='w-full flex flex-row items-center'>
+              <div className='h-8 w-8 rounded-full bg-white text-center flex justify-center items-center '>
+                {curUser?.userName.substring(0, 2).toUpperCase()}
+              </div>
+              <p className='text-xs text-start pl-2'>{curUser?.userName}</p>
+              <p className='text-[8px] text-start pl-2'>
+                {formatNumberWithCommas(curUser?.totalPoints.toFixed(2)) +
+                  ' BUFFYS'}
               </p>
             </div>
-            <div className="text-xs text-center flex justify-center w-[15%]">
-                    {
-                      ((ranking + 1) == 1 || (ranking + 1) == 2 || (ranking + 1) == 3) ?
-                      <img
-                        src={`/rank_${ranking + 1}.png`}
-                        alt="rank"
-                        className="w-4 h-6"
-                      />:
-                      "#" + (ranking + 1)
-                    }
-                    </div>
+            <div className='text-xs text-center flex justify-center w-[15%]'>
+              {ranking + 1 == 1 || ranking + 1 == 2 || ranking + 1 == 3 ? (
+                <img
+                  src={`/rank_${ranking + 1}.png`}
+                  alt='rank'
+                  className='w-4 h-6'
+                />
+              ) : (
+                '#' + (ranking + 1)
+              )}
+            </div>
           </div>
         )}
-        {
-          loading ? (
-            <div className="w-full flex items-center justify-center">
-              <Loader width="30" />
+        {loading ? (
+          <div className='w-full flex items-center justify-center'>
+            <Loader width='30' />
+          </div>
+        ) : (
+          <div>
+            <div className='flex pt-3 pb-1 text-base w-full text-[#acacac] items-center'>
+              <div className='text-center pl-2'>46M</div>
+              <div className='text-center pl-2'>holders</div>
             </div>
-          ) : (
-            <div>
-              <div className="flex pt-3 pb-1 text-base w-full text-[#acacac] items-center">
-                <div className="text-center pl-2">46M</div>
-                <div className="text-center pl-2">holders</div>
-              </div>
-              <div className="h-[62vh] overflow-auto w-full">
-                {users?.map((iUser: any, index) => (
-                  <div
-                    key={index}
-                    className={`flex px-2 py-1 items-center text-[#acacac] w-full`}
-                  >
-                    <div className="relative h-10 overflow-hidden w-[100%] flex items-center">
-                      <img
-                        src="/logo.png"
-                        alt="avatar"
-                        className="w-7 h-7 rounded-full bg-white"
-                      />
-                      <div className="pl-4 text-start">
-                        <p className="text-xs">{iUser?.userName}</p>
-                        <p className="text-[8px]">
-                          {formatNumberWithCommas(iUser?.totalPoints.toFixed(2))+" BUFFYS"}
-                        </p>
-                      </div>
+            <div className='h-[62vh] overflow-auto w-full'>
+              {users?.map((iUser: any, index) => (
+                <div
+                  key={index}
+                  className={`flex px-2 py-1 items-center text-[#acacac] w-full`}
+                >
+                  <div className='relative h-10 overflow-hidden w-[100%] flex items-center'>
+                    <div className='h-8 w-8 rounded-full bg-white text-center flex justify-center items-center '>
+                      {iUser?.userName.substring(0, 2).toUpperCase()}
                     </div>
-                    <div className="text-xs text-center flex justify-center w-[15%]">
-                      {
-                        ((index + 1) == 1 || (index + 1) == 2 || (index + 1) == 3) ?
-                        <img
-                          src={`/rank_${index + 1}.png`}
-                          alt="rank"
-                          className="w-4 h-6"
-                        />:
-                        "#" + (index + 1)
-                      }
-                      </div>
-                  </div>
-                ))}
-                {/* ------------------------------------------------------------------ */}
-                {users?.map((iUser: any, index) => (
-                  <div
-                    key={index}
-                    className={`flex px-2 py-1 items-center text-[#acacac] w-full`}
-                  >
-                    <div className="relative h-10 overflow-hidden w-[100%] flex items-center">
-                      <img
-                        src="/logo.png"
-                        alt="avatar"
-                        className="w-7 h-7 rounded-full bg-white"
-                      />
-                      <div className="pl-4 text-start">
-                        <p className="text-xs">{iUser?.userName}</p>
-                        <p className="text-[8px]">
-                          {formatNumberWithCommas(iUser?.totalPoints.toFixed(2))+" BUFFYS"}
-                        </p>
-                      </div>
+                    <div className='pl-4 text-start'>
+                      <p className='text-xs'>{iUser?.userName}</p>
+                      <p className='text-[8px]'>
+                        {formatNumberWithCommas(iUser?.totalPoints.toFixed(2)) +
+                          ' BUFFYS'}
+                      </p>
                     </div>
-                    <div className="text-xs text-center flex justify-center w-[15%]">
-                      {
-                        ((index +2) == 1 || (index +2) == 2 || (index +2) == 3) ?
-                        <img
-                          src={`/rank_${index +2}.png`}
-                          alt="rank"
-                          className="w-4 h-6"
-                        />:
-                        "#" + (index +2)
-                      }
-                      </div>
                   </div>
-                ))}
-                {users?.map((iUser: any, index) => (
-                  <div
-                    key={index}
-                    className={`flex px-2 py-1 items-center text-[#acacac] w-full`}
-                  >
-                    <div className="relative h-10 overflow-hidden w-[100%] flex items-center">
+                  <div className='text-xs text-center flex justify-center w-[15%]'>
+                    {index + 1 == 1 || index + 1 == 2 || index + 1 == 3 ? (
                       <img
-                        src="/logo.png"
-                        alt="avatar"
-                        className="w-7 h-7 rounded-full bg-white"
+                        src={`/rank_${index + 1}.png`}
+                        alt='rank'
+                        className='w-4 h-6'
                       />
-                      <div className="pl-4 text-start">
-                        <p className="text-xs">{iUser?.userName}</p>
-                        <p className="text-[8px]">
-                          {formatNumberWithCommas(iUser?.totalPoints.toFixed(2))+" BUFFYS"}
-                        </p>
-                      </div>
-                    </div>
-                    <div className="text-xs text-center flex justify-center w-[15%]">
-                      {
-                        ((index +3) == 1 || (index +3) == 2 || (index +3) == 3) ?
-                        <img
-                          src={`/rank_${index +3}.png`}
-                          alt="rank"
-                          className="w-4 h-6"
-                        />:
-                        "#" + (index +3)
-                      }
-                      </div>
+                    ) : (
+                      '#' + (index + 1)
+                    )}
                   </div>
-                ))}
-                {users?.map((iUser: any, index) => (
-                  <div
-                    key={index}
-                    className={`flex px-2 py-1 items-center text-[#acacac] w-full`}
-                  >
-                    <div className="relative h-10 overflow-hidden w-[100%] flex items-center">
-                      <img
-                        src="/logo.png"
-                        alt="avatar"
-                        className="w-7 h-7 rounded-full bg-white"
-                      />
-                      <div className="pl-4 text-start">
-                        <p className="text-xs">{iUser?.userName}</p>
-                        <p className="text-[8px]">
-                          {formatNumberWithCommas(iUser?.totalPoints.toFixed(2))+" BUFFYS"}
-                        </p>
-                      </div>
-                    </div>
-                    <div className="text-xs text-center flex justify-center w-[15%]">
-                      {
-                        ((index +4) == 1 || (index +4) == 2 || (index +4) == 3) ?
-                        <img
-                          src={`/rank_${index +4}.png`}
-                          alt="rank"
-                          className="w-4 h-6"
-                        />:
-                        "#" + (index +4)
-                      }
-                      </div>
-                  </div>
-                ))}
-                {users?.map((iUser: any, index) => (
-                  <div
-                    key={index}
-                    className={`flex px-2 py-1 items-center text-[#acacac] w-full`}
-                  >
-                    <div className="relative h-10 overflow-hidden w-[100%] flex items-center">
-                      <img
-                        src="/logo.png"
-                        alt="avatar"
-                        className="w-7 h-7 rounded-full bg-white"
-                      />
-                      <div className="pl-4 text-start">
-                        <p className="text-xs">{iUser?.userName}</p>
-                        <p className="text-[8px]">
-                          {formatNumberWithCommas(iUser?.totalPoints.toFixed(2))+" BUFFYS"}
-                        </p>
-                      </div>
-                    </div>
-                    <div className="text-xs text-center flex justify-center w-[15%]">
-                      {
-                        ((index +5) == 1 || (index +5) == 2 || (index +5) == 3) ?
-                        <img
-                          src={`/rank_${index +5}.png`}
-                          alt="rank"
-                          className="w-4 h-6"
-                        />:
-                        "#" + (index +5)
-                      }
-                      </div>
-                  </div>
-                ))}
-                {/* ------------------------------------------------------------------ */}
-              </div>
+                </div>
+              ))}
             </div>
-          )
-        }
-        
+          </div>
+        )}
       </div>
     </div>
   )
 }
 
-export default Leaderboard;
+export default Leaderboard
