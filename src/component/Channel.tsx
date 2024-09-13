c77746d4dd0f229dccde63af4dad444b80e47f77
JamesLee <jameslee82027@gmail.com>
2024-09-13 15:34:08 +0900

    fix:image size change in home page

--- src/component/Channel.tsx
+++ src/component/Channel.tsx
@@ -4,7 +4,7 @@ interface ChannelProps {
   comment: String
   src: any
 }
-const Channel: React.FC<ChannelProps> = ({ title, comment }) => {
+const Channel: React.FC<ChannelProps> = ({ title, comment, src }) => {
   return (
     <div className='bg-[#4b37dd] rounded-[1rem] w-full min-w-[18rem] flex flex-col py-2 px-[20px] justify-between'>
       <div className='flex flex-col justify-between'>
@@ -16,7 +16,7 @@ const Channel: React.FC<ChannelProps> = ({ title, comment }) => {
         <button className='bg-white rounded-3xl text-[#4b37dd] w-[100px] h-8 p-0'>
           Join
         </button>
-        <img src='/Invite_friend.png'></img>
+        <img src={src} className='w-10 h-10'></img>
       </div>
     </div>
   )
