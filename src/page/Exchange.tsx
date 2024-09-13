bce67948f318871a3b951064295c92c5c96308b8
hirotanaka0718 <hirotanaka0718@gmail.com>
2024-09-13 23:13:28 +0900

    fix: slide slash

--- src/page/Exchange.tsx
+++ src/page/Exchange.tsx
@@ -1,65 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import ProgressBar from '../component/ProgressBar'
-import Loader from '../component/Loader'
-import axios from 'axios'
-import { ENDPOINT } from '../data'
-import ExchangeSelector from '../component/exchangeSelector'
-import io from 'socket.io-client'
-import { slicFunc } from '../utils/functions'
-import Setting from './Setting'
-import Channel from '../component/Channel'
-import { Carousel } from 'flowbite-react'
+import React, { useEffect, useState } from "react";
+// import ProgressBar from '../component/ProgressBar'
+// import Loader from '../component/Loader'
+import axios from "axios";
+import { ENDPOINT } from "../data";
+// import ExchangeSelector from '../component/exchangeSelector'
+import io from "socket.io-client";
+// import { slicFunc } from '../utils/functions'
+import Setting from "./Setting";
+import Channel from "../component/Channel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import "../App.css";
+
 const ChannelData = [
   {
-    id: 'Channel',
-    title: 'DAILY TASK',
-    comment: 'Lorem Ipsum',
-    src: '/task_check.png'
+    id: "Channel",
+    title: "DAILY TASK",
+    comment: "Lorem Ipsum",
+    src: "/task_check.png",
   },
   {
-    id: 'Channel',
-    title: 'INVITE FRIENDS',
-    comment: 'Lorem Ipsum',
-    src: '/Invite_friend.png'
+    id: "Channel",
+    title: "INVITE FRIENDS",
+    comment: "Lorem Ipsum",
+    src: "/Invite_friend.png",
   },
   {
-    id: 'Channel',
-    title: 'BUFFY COMMUNITY',
-    comment: 'Lorem Ipsum',
-    src: '/Invite_friends.png'
-  }
-]
+    id: "Channel",
+    title: "BUFFY COMMUNITY",
+    comment: "Lorem Ipsum",
+    src: "/Invite_friends.png",
+  },
+];
 
-const socket = io(ENDPOINT) // Replace with your server's URL
+const socket = io(ENDPOINT); // Replace with your server's URL
 
 interface IHomeProps {
-  user: any
-  photo_url: string | null
-  point: number
-  totalPoint: number
-  handleMining: () => void
-  handleStopMining: () => void
-  setTotalPoint: (value: number) => void
-  setClaimShow: (status: boolean) => void
-  start: boolean
-  hour: number
-  min: number
-  sec: number
-  claimShow: boolean
-  power: any
-  timeLimit: any
-  level: any
-  nextLevel: any
-  loading: boolean
-  reachDailyLimit: boolean
-  setReachDailyLimit: (status: boolean) => void
-  setting: any
-  exchange: any
-  setExchange: (status: any) => void
-  tab: string
-  setTab: (status: string) => void
-  title: string
-  setTitle: (status: string) => void
+  user: any;
+  photo_url: string | null;
+  point: number;
+  totalPoint: number;
+  handleMining: () => void;
+  handleStopMining: () => void;
+  setTotalPoint: (value: number) => void;
+  setClaimShow: (status: boolean) => void;
+  start: boolean;
+  hour: number;
+  min: number;
+  sec: number;
+  claimShow: boolean;
+  power: any;
+  timeLimit: any;
+  level: any;
+  nextLevel: any;
+  loading: boolean;
+  reachDailyLimit: boolean;
+  setReachDailyLimit: (status: boolean) => void;
+  setting: any;
+  exchange: any;
+  setExchange: (status: any) => void;
+  tab: string;
+  setTab: (status: string) => void;
+  title: string;
+  setTitle: (status: string) => void;
 }
 const Exchange: React.FC<IHomeProps> = ({
   user,
@@ -88,50 +91,50 @@ const Exchange: React.FC<IHomeProps> = ({
   tab,
   setTab,
   title,
-  setTitle
+  setTitle,
 }) => {
-  const [totalUsers, setTotalUsers] = useState<number>(0)
-  const [showSetting, setShowSetting] = useState<boolean>(false)
+  const [totalUsers, setTotalUsers] = useState<number>(0);
+  const [showSetting, setShowSetting] = useState<boolean>(false);
 
   const handleClaim = () => {
     if (user) {
       axios
         .put(`${ENDPOINT}/api/user/${user?.id}`, {
           points: point,
-          countDown: 0
+          countDown: 0,
         })
-        .then(res => {
-          console.log('res', res.data)
-          let newpoint = point + totalPoint
-          setTotalPoint(newpoint)
-          setClaimShow(false)
-          setReachDailyLimit(true)
+        .then((res) => {
+          console.log("res", res.data);
+          let newpoint = point + totalPoint;
+          setTotalPoint(newpoint);
+          setClaimShow(false);
+          setReachDailyLimit(true);
         })
-        .catch(err => {
-          console.error(err)
+        .catch((err) => {
+          console.error(err);
           // toast("Something Went Wrong!");
-        })
+        });
     }
-  }
+  };
 
   useEffect(() => {
     axios
       .get(`${ENDPOINT}/api/user/totalcount`)
-      .then(res => {
-        setTotalUsers(res.data.totalCount)
+      .then((res) => {
+        setTotalUsers(res.data.totalCount);
       })
-      .catch(err => {
-        console.error(err)
+      .catch((err) => {
+        console.error(err);
         // toast("Something Went Wrong!");
-      })
-    socket.on('newUserRegistered', data => {
-      setTotalUsers(data.totalCount)
-    })
+      });
+    socket.on("newUserRegistered", (data) => {
+      setTotalUsers(data.totalCount);
+    });
 
     return () => {
-      socket.off('newUserRegistered') // Remove the listener to prevent memory leaks
-    }
-  }, [])
+      socket.off("newUserRegistered"); // Remove the listener to prevent memory leaks
+    };
+  }, []);
 
   return showSetting ? (
     <Setting
@@ -142,38 +145,50 @@ const Exchange: React.FC<IHomeProps> = ({
       setExchange={setExchange}
     />
   ) : (
-    <div className='flex flex-col h-full justify-between pt-[2.5rem] pb-[1rem] px-[20px] gap-2'>
+    <div className="flex flex-col h-full justify-between pt-[2.5rem] pb-[1rem] px-[20px] gap-2">
       <p>You’re user #100,000 to join the BuffyDrop!</p>
-      <img src='/coat.png' className='mx-[40px] h-60'></img>
+      <img src="/coat.png" className="mx-[40px] h-60"></img>
       <div>
-        <p className='text-[34px]'>16, 588</p>
-        <p className='text-[20px]'>$BUFFY</p>
+        <p className="text-[34px]">16, 588</p>
+        <p className="text-[20px]">$BUFFY</p>
       </div>
-      <div className='flex flex-row gap-2 overflow-auto w-full '>
-        {/* <Carousel indicators={true}> */}
-        {ChannelData.map((idx, key) => (
-          <Channel
-            id={idx.id}
-            heading={idx.title}
-            comment={idx.comment}
-            src={idx.src}
-            key={key}
-            tab={tab}
-            setTab={setTab}
-            title={title}
-            setTitle={setTitle}
-          />
-        ))}
-        {/* <Channel title='BUFFY COMMUNITY' comment={'Lepurm'} src='src' />
+      <div className="flex flex-row gap-2 w-full">
+        <Carousel
+          infiniteLoop = {false}
+          showArrows={false}
+          showIndicators={true}
+          showStatus={false}
+          showThumbs={false}
+          useKeyboardArrows={false}
+          autoPlay={false}
+          stopOnHover={false}
+          swipeable={true}
+          dynamicHeight={false}
+          emulateTouch={true}
+          autoFocus={false}
+        >
+          {ChannelData.map((idx, key) => (
+            <Channel
+              id={idx.id}
+              heading={idx.title}
+              comment={idx.comment}
+              src={idx.src}
+              key={key}
+              tab={tab}
+              setTab={setTab}
+              title={title}
+              setTitle={setTitle}
+            />
+          ))}
+          {/* <Channel title='BUFFY COMMUNITY' comment={'Lepurm'} src='src' />
         <Channel title='BUFFY COMMUNITY' comment={'Lepurm'} src='src' />
         <Channel title='BUFFY COMMUNITY' comment={'Lepurm'} src='src' /> */}
-        {/* </Carousel> */}
+        </Carousel>
       </div>
-
-      <button className='bg-[#4b37dd] w-full h-[2.5rem] leading-none mt-4'>
+      <button className="bg-[#110d33] w-full h-[2.5rem] leading-none mt-1">
         Claim hint
       </button>
     </div>
-  )
-}
-export default Exchange
+  );
+};
+export default Exchange;
