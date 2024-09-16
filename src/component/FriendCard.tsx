1fb3d07f01ca16650ba115b52f8b467a2517da6c
HopeStar4453 <jameslee82027@gmail.com>
2024-09-16 09:40:48 +0900

    fix:friend card avatar

--- src/component/FriendCard.tsx
+++ src/component/FriendCard.tsx
@@ -6,7 +6,7 @@ const FriendCard: React.FC<FriendCardProps> = ({ name, value }) => {
   return (
     <div className='flex flex-row items-center justify-between w-full px-6 text-[#acacac] my-2'>
       <div className='flex flex-row items-center'>
-        <img src='friend-icon.svg' alt='' className='w-8 h-8' />
+        {/* <img src='friend-icon.svg' alt='' className='w-8 h-8' /> */}
         <div className='h-8 w-8 rounded-full bg-white text-center flex justify-center items-center '>
           {name.substring(0, 2).toUpperCase()}
         </div>
