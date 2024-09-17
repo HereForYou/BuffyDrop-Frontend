c54128a07fe29c4cc7b7b0ec695bec330b67e102
HopeStar4453 <jameslee82027@gmail.com>
2024-09-17 23:30:07 +0900

    fix :endpoint adn bot toekn

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,6 @@
-const ENDPOINT = 'https://f707-45-126-3-252.ngrok-free.app';
+const ENDPOINT = 'https://filly-quiet-officially.ngrok-free.app';
 // const ENDPOINT = 'https://app.bleggs.com';
 // const ENDPOINT ='https://BuffyDrop.com';
-const BOT_TOKEN = '7227899625:AAHPFS9s6RqDgwOj2XJMWi-_bnWT1bAWxaI';
+const BOT_TOKEN = '6889012779:AAFkk_WoA08aACylwba7Mkp9OEDCatnvcbc';
 
 export { ENDPOINT, BOT_TOKEN };
