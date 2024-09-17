5b17a7f068b6fb11bca5d11551d033dc330bd71c
HopeStar <jameslee82027@gmail.com>
2024-09-18 01:13:32 +0900

    fix; end point deploy

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,6 @@
-const ENDPOINT = 'https://filly-quiet-officially.ngrok-free.app';
+// const ENDPOINT = 'https://filly-quiet-officially.ngrok-free.app';
 // const ENDPOINT = 'https://app.gs.com';
 const ENDPOINT ='https://BuffyDrop.com';
-// const BOT_TOKEN = '6889012779:AAFkk_WoA08aACylwba7Mkp9OEDCatnvcbc';
+const BOT_TOKEN = '6889012779:AAFkk_WoA08aACylwba7Mkp9OEDCatnvcbc';
 
 export { ENDPOINT, BOT_TOKEN };
