e0e2cbab9f9fc4321a09dba0b9d1f327b5dcf755
HopeStar4453 <jameslee82027@gmail.com>
2024-09-17 18:17:05 +0900

    complete

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,6 @@
-const ENDPOINT = 'https://filly-quiet-officially.ngrok-free.app';
+// const ENDPOINT = 'https://filly-quiet-officially.ngrok-free.app';
 // const ENDPOINT = 'https://app.bleggs.com';
-// const ENDPOINT ='https://BuffyDrop.com';
-const BOT_TOKEN = '7233907096:AAFXzUYCRmFFN9jJmnNE-gL56awzOh9heCs';
+const ENDPOINT ='https://BuffyDrop.com';
+const BOT_TOKEN = '6889012779:AAFkk_WoA08aACylwba7Mkp9OEDCatnvcbc';
 
 export { ENDPOINT, BOT_TOKEN };
