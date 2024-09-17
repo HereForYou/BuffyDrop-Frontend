8d9f1b097920296d65e9f9b4ea08a554d2591bbf
HopeStar4453 <jameslee82027@gmail.com>
2024-09-17 21:11:18 +0900

    fix:endpoint

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-const ENDPOINT = 'https://filly-quiet-officially.ngrok-free.app';
+const ENDPOINT = 'https://f707-45-126-3-252.ngrok-free.app';
 // const ENDPOINT = 'https://app.bleggs.com';
 // const ENDPOINT ='https://BuffyDrop.com';
 const BOT_TOKEN = '6889012779:AAFkk_WoA08aACylwba7Mkp9OEDCatnvcbc';
