502f3142b37dbd5b6f39ea66c9e5109ff12c3d0d
HereForYou <lucaslee.dev.infor@gmail.com>
2024-09-19 13:47:29 +0900

    chore: remove unnecessary imatges

--- src/component/WalletConnect.tsx
+++ src/component/WalletConnect.tsx
@@ -7,12 +7,8 @@ const WalletConnect: React.FC = () => {
   const addr = useTonAddress();
   console.log("addr", addr);
   return (
-    
       <TonConnectButton />
   );
 };
 
 export default WalletConnect;
-
-// {/* className="sticky top-0 z-1 flex w-full items-center justify-center" */}
-// {/* <TonConnectButton /> */}
