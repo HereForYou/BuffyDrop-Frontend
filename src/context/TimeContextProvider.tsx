947574d2940436f7108f42507031d8ff2c5705d8
HereForYou <lucaslee.dev.infor@gmail.com>
2024-09-21 03:28:51 +0900

    feat: add earning part every specific time

--- src/context/TimeContextProvider.tsx
+++ src/context/TimeContextProvider.tsx
@@ -2,29 +2,44 @@ import React, { useState, createContext, ReactNode, useContext } from "react";
 
 interface ITimeContext {
   minedAmount: number;
+  notReceiveAmount: number;
   remainTime: number;
   setMinedAmount: React.Dispatch<React.SetStateAction<number>>;
+  setNotReceivedAmount: React.Dispatch<React.SetStateAction<number>>;
   setRemainTime: React.Dispatch<React.SetStateAction<number>>;
+  setTotalTime: React.Dispatch<React.SetStateAction<number>>;
+  setUserId: React.Dispatch<React.SetStateAction<string>>;
+  totalTime: number;
+  userId: string;
 }
 
 interface ITimeContextProvider {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-export const TimeContext = createContext<ITimeContext | undefined>(
-  undefined
-);
+export const TimeContext = createContext<ITimeContext | undefined>(undefined);
 
-export const TimeContextProvider : React.FC<ITimeContextProvider> = ({ children }) => {
-  const [remainTime, setRemainTime] = useState(0);
+export const TimeContextProvider: React.FC<ITimeContextProvider> = ({
+  children,
+}) => {
+  const [userId, setUserId] = useState("");
+  const [totalTime, setTotalTime] = useState(100000000);
+  const [remainTime, setRemainTime] = useState(1);
   const [minedAmount, setMinedAmount] = useState(0);
+  const [notReceiveAmount, setNotReceivedAmount] = useState(0);
   return (
     <TimeContext.Provider
       value={{
         minedAmount,
+        notReceiveAmount,
         remainTime,
         setMinedAmount,
+        setNotReceivedAmount,
         setRemainTime,
+        setTotalTime,
+        setUserId,
+        totalTime,
+        userId,
       }}>
       {children}
     </TimeContext.Provider>
