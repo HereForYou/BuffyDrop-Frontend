9228e98612597be3cc167425ee2ce57f4670cc77
HopeStar <jameslee82027@gmail.com>
2024-09-24 01:31:25 +0900

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
 // const ENDPOINT ='https://BuffyDrop.com';// Deployed Backend server domain 
-const ENDPOINT ='http://lucas-lee.top';// test local 
+const ENDPOINT ='https://lucas-lee.top';// test local 
 // const ENDPOINT ='https://filly-quiet-officially.ngrok-free.app';// Test local Backend server domain 
 // const ENDPOINT ='http://192.168.142.158:5000';// Test local Backend server domain 
 // const BOT_TOKEN = '7233907096:AAFXzUYCRmFFN9jJmnNE-gL56awzOh9heCs';//Telegram Mini App Test bot token
