5659f06f7afdfe7af675d8fa3366d4ba7dd38960
HopeStar <jameslee82027@gmail.com>
2024-09-24 01:24:49 +0900

    bot token chage

--- src/data.ts
+++ src/data.ts
@@ -3,6 +3,6 @@ const ENDPOINT ='http://lucas-lee.top';// test local
 // const ENDPOINT ='https://filly-quiet-officially.ngrok-free.app';// Test local Backend server domain 
 // const ENDPOINT ='http://192.168.142.158:5000';// Test local Backend server domain 
 // const BOT_TOKEN = '7233907096:AAFXzUYCRmFFN9jJmnNE-gL56awzOh9heCs';//Telegram Mini App Test bot token
-const BOT_TOKEN = '6889012779:AAFC_s_njdefG4RsN__d7ZBarzfPnlUEKro';//Telegram Mini App bot token
+const BOT_TOKEN = '7233907096:AAFXzUYCRmFFN9jJmnNE-gL56awzOh9heCs';//Telegram Mini App bot token
 
 export { ENDPOINT, BOT_TOKEN };
