7ade632a367fea8f0b4a226eb17b41e84c4ca9b0
HereForYou <lucaslee.dev.infor@gmail.com>
2024-09-26 21:31:37 +0900

    fix: remove double click part from joinBuffyModal.tsx

--- src/component/JoinBuffyModal.tsx
+++ src/component/JoinBuffyModal.tsx
@@ -9,10 +9,12 @@ const JoinBuffyModal: React.FC<IJoinBuffyModalProps> = ({ setIsModal }) => {
   return (
     <div
       className='w-full h-full bg-opacity-0 z-0 absolute top-0 flex justify-center items-center'
-      onDoubleClick={() => setIsModal(false)}>
+      // onDoubleClick={() => setIsModal(false)}
+    >
       <div
         className='z-50 flex bg-black flex-col gap-2 justify-center items-center max-w-96 w-10/12 font-consolas text-white py-5 h-fit border border-gray-600 rounded-3xl'
-        onDoubleClick={(event) => event.stopPropagation()}>
+        // onDoubleClick={(event) => event.stopPropagation()}
+      >
         <div>
           <img src='/modal.png' alt='modal' />
         </div>
