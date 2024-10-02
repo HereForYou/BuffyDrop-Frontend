f47bde5cfaf59259c300e2510d289fee449bc0df
HereForYou <163456746+HereForYou@users.noreply.github.com>
2024-10-02 12:05:32 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ import NotFound from "./page/NotFount";
 // const start_param = "";
 
 function App() {
+  console.log("endpoint",ENDPOINT)
   const hasShownWarningRef = useRef(false);
   const { user, start_param } = useTelegram();
   const [inviteMsg, setInviteMsg] = useState<boolean>(false);
