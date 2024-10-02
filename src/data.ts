e1d16e25c23e473b522c9b3a22be72e5c609370b
HereForYou <163456746+HereForYou@users.noreply.github.com>
2024-10-02 11:56:59 +0900

    change endpoint

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
 // const ENDPOINT ='https://BuffyDrop.com';// Deployed Backend server domain 
-const ENDPOINT ='https://smashing-hyena-cute.ngrok-free.app';// Test local Backend server domain mine
+const ENDPOINT ='https://smashing-hyena-cute.ngrok-free.app';
 // const ENDPOINT ='http://192.168.142.156:5000';// Test local Backend server domain 
 // const BOT_TOKEN = '6889012779:AAGeT2TE5ThX60rEBqZZAr-XzeG6Uh0NUdY';//Deployed Bot Token BuffyDrop bot token
 // const BOT_TOKEN = '7588954052:AAFpS8kJN1M2xDGw3MmICaWTA_f_eZXKGTA';//Test Bot Token7588954052:AAE7WpCDVCq_0s3T81GkPEnuNITHacxxJdc
